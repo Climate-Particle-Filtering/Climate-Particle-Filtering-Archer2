60eb6a674240ae3072385dbb58e47572b0ff148b
not populated <m.mineter@ed.ac.uk>
2023-03-13 19:47:45 +0000

    the function filter does the filtering: 1. select runs whose dumpsr are to be used 2. assign them to the runs to be continued 3. apply them 4. return the list of tuples so what was done is remembered
    This is just a dummy that supports testing.

---
+++ pf_a2/dummy_filter.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#
+# Copyright (c) The University of Edinburgh, 2014.
+#
+"""
+exemplar of a filter function
+inputs:
+    runlist - input subdirectories
+    topdir: their parent directoru
+output:
+    list of tuples {runname, run used to overwirte its dump, dict of how that overwirte was done)
+        eg. [(zd001, zd004, {perturbation: x_for_zd001}),
+             (zd002. zd004, {perturbation: x_for zd002}),
+             .... ]
+             so here zd004 dumps were twice perturbed to continue the runs zd001 and 2.
+"""
+def filter(runlist,topdir):
+    # select runs whose dumps are to be used
+    # for the run in the corresponding input runlist item
+    # if there is anything to remember about how the dumps are perturbed,
+    # put it int he corresponding list of dictionaries
+
+    # this dummy function
+    #    - let the filtering list just be an inverted list.
+    #    - invent a list of dicts in case anythign to be remembered for each perturbation
+
+    filteredlist=runlist[::-1]
+
+    adictlist=[]
+
+    for (i,item) in enumerate(filteredlist):
+        adictlist.append({'dummypar':i/1000.})
+
+    oittuples = list(zip(runlist, filteredlist,adictlist))
+
+
+    # in real world for each tuple overwrite the necessary dump.
+    # in this dummy case we assume that is done and just return the list of tuples
+    # its used just to hold a record of what was done
+
+    return oittuples;
